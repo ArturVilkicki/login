dae678fa9fb99b713d00f9ce4296531433c85d47
ArturVilkicki <30698622+ArturVilkicki@users.noreply.github.com>
2017-09-05 20:08:50 +0300

    initial setup

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+$vardas = "";
+
+//Cokies
+//setcookie("username", "Pranas", time() + (60 * 60 * 24 * 7), "/");
+/*
+if(isset($_COOKIE["username"])) {
+    $vardas = $_COOKIE["username"];
+} else {
+	$vardas = "nezinomas";
+}
+*/
+
+//Sessions
+/*
+session_start();
+
+//$_SESSION['username'] = "Antanas";
+$vardas = $_SESSION['username'];
+
+if (isset($_SESSION["username"])) {
+	$vardas = $_SESSION['username'];
+} else {
+	$vardas = "nezinomas";
+}
+*/
+
+include "view/login.php";
+include "view/main.php";
